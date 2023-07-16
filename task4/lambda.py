c30684baf2915a92109d40d64bb7c32db7c180fc
Pratyush Asthana <114195124+melenar@users.noreply.github.com>
2023-07-16 19:56:55 +0530

    Add files via upload

--- task4/lambda.py
+++ task4/lambda.py
@@ -4,11 +4,10 @@ import boto3
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
 
-    s3_bucket = event['Records'][0]['s3']['bucket']['name']
-    file_name = event['Records'][0]['s3']['object']['key']
+    s3_bucket = "bucketofexample"
+    file_name = "email.txt"
 
     response = s3.get_object(Bucket=s3_bucket, Key=file_name)
-    content = response['Body'].read().decode('utf-8')
+    emails = response['Body'].read().decode('utf-8').splitlines()
 
-    body="The Link to your bucket object is"+"https://"+s3_bucket+".s3.us-east-1.amazonaws.com/"+file_name+"\n \n Contents:\n"+content
-    return body
+    return emails
